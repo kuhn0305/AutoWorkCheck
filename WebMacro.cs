ee7d7cc731e8d33dfdea4db064a2d29b8e369f29
Hyunsuk Lee <leehs27th@gmail.com>
2021-06-11 14:05:50 +0900

    Update WebMacro.cs
    Handle Alert

--- WebMacro.cs
+++ WebMacro.cs
@@ -32,30 +32,39 @@ namespace AutoWorkCheck
 
         public void Run(string id, string password, bool isAttend)
         {
-            driver = new ChromeDriver(driverService, options);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-            driver.Navigate().GoToUrl("http://gw.tektonspace.com/"); // 웹 사이트에 접속합니다.
-            driver.SwitchTo().Frame(driver.FindElementByXPath("/html/frameset/frame"));
-
-            IWebElement idTextBox = driver.FindElementByXPath("//*[@id=\"loginForm\"]/div/table/tbody/tr[1]/td[2]/input");
-            idTextBox.SendKeys(id);
-
-            IWebElement passwordTestBox = driver.FindElementByXPath("//*[@id=\"loginForm\"]/div/table/tbody/tr[2]/td[2]/input");
-            passwordTestBox.SendKeys(password);
-
-            IWebElement loginButton = driver.FindElementByXPath("//*[@id=\"loginFormSubmit\"]");
-            loginButton.Click();
-
-            if(isAttend)
+            try
             {
-                IWebElement checkButton = driver.FindElementByXPath("//*[@id=\"contentsdivAttendanceInfo\"]/div/div[2]/p[2]/a");
-                checkButton.Click();
+                driver = new ChromeDriver(driverService, options);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                driver.Navigate().GoToUrl("http://gw.tektonspace.com/"); // 웹 사이트에 접속합니다.
+                driver.SwitchTo().Frame(driver.FindElementByXPath("/html/frameset/frame"));
+
+                IWebElement idTextBox = driver.FindElementByXPath("//*[@id=\"loginForm\"]/div/table/tbody/tr[1]/td[2]/input");
+                idTextBox.SendKeys(id);
+
+                IWebElement passwordTestBox = driver.FindElementByXPath("//*[@id=\"loginForm\"]/div/table/tbody/tr[2]/td[2]/input");
+                passwordTestBox.SendKeys(password);
+
+                IWebElement loginButton = driver.FindElementByXPath("//*[@id=\"loginFormSubmit\"]");
+                loginButton.Click();
+
+                if(isAttend)
+                {
+                    IWebElement checkButton = driver.FindElementByXPath("//*[@id=\"contentsdivAttendanceInfo\"]/div/div[2]/p[2]/a");
+                    checkButton.Click();
+                }
+                else
+                {
+                    IWebElement checkButton = driver.FindElementByXPath("//*[@id=\"contentsdivAttendanceInfo\"]/div/div[2]/p[3]/a");
+                    checkButton.Click();
+                }
+
+                driver.SwitchTo().Alert().Accept();
             }
-            else
+            catch(Exception e)
             {
-                IWebElement checkButton = driver.FindElementByXPath("//*[@id=\"contentsdivAttendanceInfo\"]/div/div[2]/p[3]/a");
-                checkButton.Click();
+                Console.WriteLine(e.Message);
             }
         }
     }
